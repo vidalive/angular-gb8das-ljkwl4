5b3ae818ff7b633ac70f8a6031c93ff882f4a0f1
vidalive <vidalive@gmail.com>
2020-03-10 18:47:55 -0600

    1 file modified

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -8,7 +8,9 @@ import { products } from '../products';
   styleUrls: ['./product-details.component.css']
 })
 export class ProductDetailsComponent implements OnInit {
-product;
+addToCart(product) {
+  this.cartService.addToCart(product);
+}
 
   constructor(
     private route: ActivatedRoute,
